fd13a763720b8c36bea952d74bcbc3912e7b1ebe
권찬호 <rnjscksdud17@gmail.com>
2024-12-09 14:33:00 +0900

    초기 커밋.

---
+++ G24W15WPFDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Windows;
+
+namespace G24W15WPFDataGrid;
+
+public partial class MainWindow : Window
+{
+    protected List<GundamModel> GundamList = new List<GundamModel>();
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        GundamGrid.ItemsSource = GundamList;
+
+        GundamList.Add(new GundamModel("건담", "RX-78-2", "연방군"));
+        GundamList.Add(new GundamModel("자쿠II", "MS-06", "지온군"));
+    }
+
+    public void OnAdd(object sender, RoutedEventArgs e)
+    {
+        GundamList.Add(new GundamModel("건탱크", "RX-75-4", "연방군"));
+        
+    }
+}
